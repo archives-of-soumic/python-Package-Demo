ce4fe79dd0fe9494892baf620aca9d88107894a7
fahimfarhan <fahim.farhan@outlook.com>
2019-04-14 19:23:18 +0600

    FC auto-commit

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     # Needed to actually package something
     packages=['measure'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['numpy', 'flask'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='1.0',
     # The license can be anything you like
     license='MIT',
     description='An example of a python package from pre-existing code',
